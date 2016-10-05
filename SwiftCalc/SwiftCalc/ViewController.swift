cbe336366e150db0fbbe085df3d454787ac36cad
Michael Ma <michaelma1210@gmail.com>
2016-10-05 12:54:48 -0700

    10-5-16 HW2 Calculator Final

--- SwiftCalc/SwiftCalc/ViewController.swift
+++ SwiftCalc/SwiftCalc/ViewController.swift
@@ -21,7 +21,11 @@ class ViewController: UIViewController {
     
     // TODO: This looks like a good place to add some data structures.
     //       One data structure is initialized below for reference.
-    var someDataStructure: [String] = [""]
+//    var someDataStructure: [String] = [""]
+    var numbersAL = [String]()
+    var operatorAL = [String]()
+    var operatorSwitch = false
+    var decimalSwitch = false
     
 
     override func viewDidLoad() {
@@ -78,19 +82,144 @@ class ViewController: UIViewController {
     
     // REQUIRED: The responder to a number button being pressed.
     func numberPressed(_ sender: CustomButton) {
+        // Fill me in!
         guard Int(sender.content) != nil else { return }
         print("The number \(sender.content) was pressed")
-        // Fill me in!
+        if numbersAL.isEmpty || operatorSwitch == true {
+            numbersAL.append(sender.content)
+            operatorSwitch = false
+
+        }
+        else {
+            if numbersAL[numbersAL.count - 1].characters.count < 7 {
+//                if decimalSwitch == true {
+//                    numbersAL[numbersAL.count - 1] += "."
+//                    decimalSwitch = false
+//                    showNewNum()
+//                }
+                numbersAL[numbersAL.count - 1] += sender.content
+            }
+        }
+        
+        showNewNum()
+        
+    }
+    
+    func turnToDecimalForm(doubleNum: Double) -> String {
+        if doubleNum.truncatingRemainder(dividingBy: 1) == 0 {
+            let decimalNum = Int(doubleNum)
+            return String(decimalNum)
+        }
+        return String(doubleNum)
+    }
+    
+    func showNewNum() {
+        resultLabel.text = numbersAL[numbersAL.count - 1]
+    }
+    
+    func doCalculate() {
+        if numbersAL.count == 1 && !operatorAL.isEmpty {
+            let first = Double(numbersAL.removeFirst())
+            let second = first
+            let op = operatorAL.removeFirst()
+            let temp = mathCalculation(firstNum: (first)!, secondNum: (second)!, operation: op)
+            let stringNum = turnToDecimalForm(doubleNum: temp)
+            numbersAL.append(stringNum)
+            showNewNum()
+        }
+        if numbersAL.count == 2 && !operatorAL.isEmpty {
+
+            let first = Double(numbersAL.removeFirst())
+            let second = Double(numbersAL.removeFirst())
+            let op = operatorAL.removeFirst()
+            let temp = mathCalculation(firstNum: (first)!, secondNum: (second)!, operation: op)
+            let stringNum = turnToDecimalForm(doubleNum: temp)
+            numbersAL.append(stringNum)
+            showNewNum()
+        }
+
+    }
+    
+    func mathCalculation(firstNum: Double, secondNum: Double, operation: String) -> Double {
+        if operation == "+" {
+            return firstNum + secondNum
+        }
+        else if operation == "-" {
+            return firstNum - secondNum
+        }
+        else if operation == "*" {
+            return firstNum * secondNum
+        }
+        else if operation == "/" {
+            return firstNum / secondNum
+        }
+        return 0
     }
     
     // REQUIRED: The responder to an operator button being pressed.
     func operatorPressed(_ sender: CustomButton) {
         // Fill me in!
+        
+        //clear the number
+        if sender.content == "C" {
+            numbersAL.removeAll()
+            operatorAL.removeAll()
+            resultLabel.text = "0"
+        }
+        
+        //switch between positive and negative
+        else if sender.content == "+/-" {
+            var currNum = Double(numbersAL.removeLast())!
+            currNum = currNum - (2 * currNum)
+            let stringNum = turnToDecimalForm(doubleNum: currNum)
+            numbersAL.append(stringNum)
+            
+            showNewNum()
+        }
+        
+        //calculate when equal sign pressed
+        else if sender.content == "=" {
+            doCalculate()
+            showNewNum()
+            
+        }
+            
+        else if sender.content == "%" {
+            var currNum = Double(numbersAL.removeLast())!
+            currNum = currNum / 100
+            let stringNum = turnToDecimalForm(doubleNum: currNum)
+            numbersAL.append(stringNum)
+            
+            showNewNum()
+        }
+            
+        else {
+
+            operatorAL.append(sender.content)
+            operatorSwitch = true
+            
+            if operatorAL.count > 1 {
+                doCalculate()
+            }
+        }
+        
+
     }
     
     // REQUIRED: The responder to a number or operator button being pressed.
     func buttonPressed(_ sender: CustomButton) {
        // Fill me in!
+        if sender.content == "0" {
+            numberPressed(sender)
+        }
+        
+        if sender.content == "." {
+            if numbersAL.isEmpty {
+                numbersAL.append("0")
+            }
+            numbersAL[numbersAL.count - 1] += "."
+            showNewNum()
+        }
     }
     
     // IMPORTANT: Do NOT change any of the code below.
